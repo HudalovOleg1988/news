fc2bbaa285544a3260cdf849753eec10b69f0989
HudalovOleg1988 <55542403+HudalovOleg1988@users.noreply.github.com>
2024-10-21 13:00:23 +0300

    Create header.blade.php

---
+++ header.blade.php
@@ -0,0 +1,13 @@
+<div class="header">
+	<div class="logo_block">
+		<a href="/">The news blog</a>
+	</div>
+	<nav>
+		<div class="sections">
+			<a href="/section/1">SPORT</a>
+			<a href="/section/2">MOVIE</a>
+			<a href="/section/3">WORLD</a>
+		</div>
+		<a href="" class="admin">ADMIN</a>
+	</nav>
+</div>
